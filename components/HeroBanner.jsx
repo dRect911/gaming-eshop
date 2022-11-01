164dfdb045d6e15d405c56f96f3724ec013e3582
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-11-01 18:51:19 +0000

    added color palette and components

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <div className="mt-1 text-[4rem] " >
+        <div>
+            <p className="" >SMALL TEXT</p>
+            <h3>MID TEXT</h3>
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default HeroBanner
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      primary:'#5124F2',
+      secondary:'#C6F224',
+      primaryLight:'#DAD3F5',
+      secondaryLight:'#EDF5D3',
+      bgDark:'#171326',
+      bgLight:'#F2F0F7',
+    }
+  },
+  plugins: [],
+}
+
+
+/* blue palette */
+/* 
+#00005a
+#000088
+#131ab9
+#E6E6FF
+#F5F5FF
+*/
