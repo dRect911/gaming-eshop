8ee91f37a73e1534170e97c042792824f4fae370
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-11-08 12:58:22 +0000

    style: tablet (mid screen) view hero banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -7,16 +7,16 @@ import { urlFor } from '../lib/client'
 
 const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className="relative rounded-2xl w-full h-[500px] py-6 md:py-10 px-6 md:px-[100px] mt-16 md:mt-20 bg-primary-light leading-none group " >
+    <div className="relative rounded-2xl w-full h-[500px] py-6 md:py-10 px-6 md:px-[70px] lg:px-[100px] mt-16 md:mt-20 bg-primary-light leading-none group " >
         <div>
             <p className="text-[16px] text-center md:text-start md:text-[20px] text-primary-dark transition-all" >{heroBanner.smallText}</p>
             <h3 className="text-[3rem] md:text-[4rem] text-center md:text-start mt-2 md:mt-1 font-semibold text-bgLight transition-all" >{heroBanner.midText}</h3>
             <h1 className="text-[6em] sm:text-[8em] md:text-[10em] text-center md:text-start md:-ml-[20px] font-bold uppercase text-primary transition-all" >{heroBanner.largeText1}</h1>
-            <img src={urlFor(heroBanner.image)} alt="keyboard" className="absolute top-72 sm:top-64 md:top-0 right-[20%] h-44 sm:h-56 md:h-[450px] -translate-x-8 group-hover:translate-x-8 hover:scale-105 hover:-skew-x-2 hover:skew-y-4 transition-all duration-1000 "  /> {/* hero-banner-image */}
+            <img src={urlFor(heroBanner.image)} alt="keyboard" className="absolute top-72 sm:top-64 md:top-32 lg:top-0 right-[20%] md:right-[5%] lg:right-[20%] h-44 sm:h-56 md:h-[280px] lg:h-[450px] -translate-x-8 group-hover:translate-x-8 hover:scale-105 hover:-skew-x-2 hover:skew-y-4 transition-all duration-1000 "  /> {/* hero-banner-image */}
 
             <div>
                 <Link href={`/product/${heroBanner.product}`} >
-                    <button type="button" className="rounded-lg py-[10px] px-4 bg-secondary text-white hover:bg-bgLight hover:text-primary-dark transition-all hover:translate-x-2 hover:font-bold ml-6 sm:ml-12 md:ml-0 mt-10 sm:mt-6 md:mt-10 text-[22px] font-medium cursor-pointer z-20 duration-300 " >
+                    <button type="button" className="rounded-lg py-[10px] px-4 md:py-[12px] md:px-6 bg-secondary text-white hover:bg-bgLight hover:text-primary-dark transition-all hover:translate-x-2 hover:font-bold ml-6 sm:ml-12 md:ml-0 mt-10 sm:mt-6 md:mt-10 text-[22px] md:text-[26px] font-medium cursor-pointer z-20 duration-300 " >
                     {heroBanner.buttonText}
                     </button>
                 </Link>
