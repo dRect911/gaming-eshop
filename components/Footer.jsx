371bda990c3fd9661c632bfdfc4e264672c32772
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-11-09 16:42:14 +0000

    style: dark mode footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,8 +5,8 @@ import { AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai'
 const Footer = () => {
   return (
     <div className="w-full p-3 md:p-4" >
-      <p className="text-primary-dark text-center text-[12px] sm:text-[15px] md:text-lg font-medium" >2022 - dRect911 All rights reserved.</p>
-      <div className="text-primary-dark text-center text-xl md:text-2xl flex items-center justify-center gap-4 p-1 md:p-2" >
+      <p className="text-primary-dark dark:text-primary-light/75 text-center text-[12px] sm:text-[15px] md:text-lg font-medium" >2022 - dRect911 All rights reserved.</p>
+      <div className="text-primary-dark dark:text-primary-light/75 text-center text-xl md:text-2xl flex items-center justify-center gap-4 p-1 md:p-2" >
         <AiFillInstagram />
         <AiFillTwitterCircle />
       </div>
