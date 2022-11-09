fc407fd04535b576f630574892972cd9b58fbe0c
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-11-09 14:35:09 +0000

    style: mobile view cart layout

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, {useRef} from 'react';
 import Link from 'next/link';
 import toast from 'react-hot-toast';
@@ -35,7 +36,7 @@ const Cart = () => {
 
   return (
     <div className="w-[100vw] bg-bgDark/50 backdrop-blur-xl fixed top-0 right-0 z-[100] transition-all " ref={cartRef} >{/* cart wrapper */}
-      <div className="relative h-[100vh] w-[600px] bg-bgLight py-10 px-2 float-right text-bgDark " >{/* cart container */}
+      <div className="relative h-[100vh] w-full md:w-[600px] bg-bgLight py-4 md:py-10 px-2 float-right text-bgDark transition-all " >{/* cart container */}
         <button type="button" onClick={() => { setShowCart(false) }} className="group flex items-center gap-0.5 ml-2 bg-transparent border-none text-lg font-medium cursor-pointer "  > {/* cart heading */}
           <HiChevronLeft />{/* className="group-hover:scale-110 transition-all" */}
           <span className="ml-2">Your Cart</span>
@@ -52,26 +53,26 @@ const Cart = () => {
           </div>
         )}
 
-        <div className=" mt-4 overflow-auto scrollbar-thin scrollbar-track-primary-light scrollbar-thumb-primary-dark scrollbar-rounded-full  max-h-[70vh] py-5 px-4 " >
+        <div className=" md:mt-4 overflow-auto scrollbar-thin scrollbar-track-primary-light scrollbar-thumb-primary-dark scrollbar-rounded-full  max-h-[70vh] py-3 md:py-5 md:px-4 " >
           {cartItems.length >= 1 && cartItems.map((item) => {
             console.log(item);
             return (
-            <div key={item._id} className="flex gap-4 p-5">
-              <img src={urlFor(item?.image[0])} alt="product-image" className="max-w-[180px] max-h-[150px] rounded-lg border border-secondary-light bg-white" />
-              <div className="py-4 w-[340px]">
+            <div key={item._id} className="flex gap-4 p-3 md:p-5">
+              <img src={urlFor(item?.image[0])} alt="product-image" className="max-w-[120px] max-h-[100px]  md:max-w-[180px] md:max-h-[150px] rounded-lg border border-secondary-light bg-white transition-all" />
+              <div className="py-4 w-full md:w-[340px]">
                 <div className="flex justify-between items-center w-full ">
-                  <h1 className="font-semibold">{item.name} </h1>
-                  <h1 className="text-primary font-bold text-xl">${item.price}</h1>
+                  <h1 className="font-semibold text-sm md:text-lg">{item.name} </h1>
+                  <h1 className="text-primary font-bold text-[16px] md:text-xl">${item.price}</h1>
                 </div>
                 <div className="mt-8 flex justify-between items-center w-full" >
                   <div className=" flex gap-5 items-center select-none" >{/* Quantity */}
                     <span className="flex items-center w-auto bg-white border border-primary-dark rounded p-1" >{/* qty desc */}
-                      <span onClick={() => {toggleCartItemQuantity(item._id, 'dec') }} className="flex shrink-0 items-center justify-center cursor-pointer  border-r  text-bgDark hover:text-primary  w-8 h-8 rounded-l transition-all"><HiMinus/></span>
-                      <span className="flex shrink-0 items-center justify-center cursor-pointer text-primary-dark w-8 h-8 ">{item.quantity}</span>
-                      <span onClick={() => {toggleCartItemQuantity(item._id, 'inc') }} className="flex shrink-0 items-center justify-center cursor-pointer border-l  text-bgDark hover:text-primary  w-8 h-8 rounded-r transition-all"><HiPlus/></span>
+                      <span onClick={() => {toggleCartItemQuantity(item._id, 'dec') }} className="flex shrink-0 items-center justify-center cursor-pointer  border-r  text-bgDark hover:text-primary  w-6 md:w-8 h-6 md:h-8 rounded-l transition-all"><HiMinus/></span>
+                      <span className="flex shrink-0 items-center justify-center cursor-pointer text-primary-dark w-6 md:w-8 h-6 md:h-8 transition-all">{item.quantity}</span>
+                      <span onClick={() => {toggleCartItemQuantity(item._id, 'inc') }} className="flex shrink-0 items-center justify-center cursor-pointer border-l  text-bgDark hover:text-primary  w-6 md:w-8 h-6 md:h-8 rounded-r transition-all"><HiPlus/></span>
                     </span>
                   </div>
-                  <button type="button" onClick={() => { onRemove(item._id) }} className="text-2xl text-secondary cursor-pointer hover:scale-105 transition-all ">
+                  <button type="button" onClick={() => { onRemove(item._id) }} className="text-lg md:text-2xl text-secondary cursor-pointer hover:scale-105 transition-all ">
                     <TiDeleteOutline />
                   </button>
                 </div>
@@ -81,13 +82,13 @@ const Cart = () => {
         </div>
 
         {cartItems.length >= 1 && (
-          <div className="sticky bottom-0 px-4 py-8 w-[592px] m-auto " >
+          <div className="sticky bottom-0 px-4 py-8 w-full md:w-[592px] m-auto " >
             <div className="flex justify-between items-center w-full" >
-              <h3 className="text-xl font-semibold" >Subtotal: </h3>
-              <h3 className="text-xl font-bold text-primary" >${totalPrice}</h3>
+              <h3 className="text-lg md:text-xl font-semibold" >Subtotal: </h3>
+              <h3 className="text-lg md:text-xl font-bold text-primary" >${totalPrice}</h3>
             </div>
             <div className="w-full flex justify-center">
-              <button type="button" onClick={handleCheckout} className="py-3 px-5 w-[300px] m-auto uppercase mt-10 text-xl cursor-pointer hover:scale-105 font-semibold border bg-white border-secondary text-secondary hover:border-secondary-dark hover:text-secondary-dark hover:bg-secondary-light hover:font-semibold transition-all rounded-lg ">Checkout with Stripe</button>
+              <button type="button" onClick={handleCheckout} className="py-2 md:py-3 px-3 md:px-5 w-[260px] md:w-[300px] m-auto uppercase mt-6 md:mt-10 text-lg md:text-xl cursor-pointer hover:scale-105 font-semibold border bg-white border-secondary text-secondary hover:border-secondary-dark hover:text-secondary-dark hover:bg-secondary-light hover:font-semibold transition-all rounded-lg ">Checkout with Stripe</button>
             </div>
           </div>
         )}
