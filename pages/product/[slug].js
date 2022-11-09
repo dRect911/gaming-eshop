a55d8392a4dfc28e82aa86ad65db229c9778db63
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-11-09 17:26:29 +0000

    style: dark mode product details

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -25,7 +25,7 @@ const ProductDetails = ({ product, products }) => {
         <div className="w-full flex flex-wrap md:flex-nowrap gap-10 justify-between mt-16 text-bgDark ">{/* product detail container */}
             <div className="md:pl-6" >
                 <div className="">{/* image container */}
-                    <img src={urlFor(image[index])} alt="product image" className="bg-bgLight hover:bg-secondary-light border border-secondary-light rounded-xl min-h-[200px] max-w-full md:min-h-[300px] md:max-w-[450px] lg:min-h-[400px] lg:max-w-[600px] md:shrink-0 transition-all " />
+                    <img src={urlFor(image[index])} alt="product image" className="bg-bgLight hover:bg-secondary-light dark:bg-bgLight/10 dark:hover:bg-bgLight/20 border border-secondary-light dark:border-primary-light/50 rounded-xl min-h-[200px] max-w-full md:min-h-[300px] md:max-w-[450px] lg:min-h-[400px] lg:max-w-[600px] md:shrink-0 transition-all " />
                 </div>
 
                 {/* mini images */}
@@ -35,7 +35,7 @@ const ProductDetails = ({ product, products }) => {
                         key={i} 
                         alt=""
                         src={urlFor(item)}
-                        className={` ${ i === index ? 'border-secondary bg-secondary' : 'border-secondary-light bg-secondary-light' } rounded-lg cursor-pointer border max-w-[50px] max-h-[50px] md:max-w-[70px] md:max-h-[70px] transition-all`}
+                        className={` ${ i === index ? 'dark:border-primary border-secondary dark:bg-primary bg-secondary' : 'dark:border-primary-light/50 border-secondary-light dark:bg-primary-light/50 bg-secondary-light' } rounded-lg cursor-pointer border max-w-[50px] max-h-[50px] md:max-w-[70px] md:max-h-[70px] transition-all`}
                         onMouseEnter={() => setIndex(i) }
                         />
                     ))}
@@ -43,35 +43,35 @@ const ProductDetails = ({ product, products }) => {
                 
             </div>
             <div className="max-w-full md:max-w-[500px] py-4 md:pr-6 " >{/* products details desc */}
-                <h1 className="text-2xl font-semibold text-primary-dark" >{name}</h1>
+                <h1 className="text-2xl font-semibold text-primary-dark dark:text-primary" >{name}</h1>
 
                 <div className="flex gap-1 items-center">{/* reviews */}
-                    <div className="flex gap-1 text-secondary" >
+                    <div className="flex gap-1 text-secondary " >
                         <HiStar/>
                         <HiStar/>
                         <HiStar/>
                         <HiStar/>
                         <HiOutlineStar/>
                     </div>
-                    <p> (27) </p>
+                    <p className="dark:text-bgLight" > (27) </p>
                 </div>
 
                 <h4 className="text-lg font-medium mt-2">Details: </h4>
-                <p className="mt-1">{details}</p>
+                <p className="mt-1 dark:text-bgLight/90">{details}</p>
                 <p className="mt-8 font-bold text-secondary text-2xl sm:text-3xl md:text-2xl transition-all ">${price}</p>{/* price */}
 
                 <div className="mt-2 flex gap-5 items-center select-none" >{/* Quantity */}
-                    <h3 className="text-lg font-medium  text-primary-dark" >Quantity: </h3>
-                    <span className="flex items-center w-auto bg-white border border-primary-dark rounded p-1" >{/* qty desc */}
-                        <span onClick={decQty} className="flex shrink-0 items-center justify-center cursor-pointer  border-r  text-bgDark hover:text-primary  w-8 h-8 rounded-l transition-all"><HiMinus/></span>
-                        <span className="flex shrink-0 items-center justify-center cursor-pointer text-primary-dark w-8 h-8 ">{qty}</span>
-                        <span onClick={incQty} className="flex shrink-0 items-center justify-center cursor-pointer border-l  text-bgDark hover:text-primary  w-8 h-8 rounded-r transition-all"><HiPlus/></span>
+                    <h3 className="text-lg font-medium  text-primary-dark dark:text-primary" >Quantity: </h3>
+                    <span className="flex items-center w-auto bg-white border dark:bg-bgLight/10 border-primary-dark dark:border-primary-light/75 rounded p-1" >{/* qty desc */}
+                        <span onClick={decQty} className="flex shrink-0 items-center justify-center cursor-pointer  border-r  text-bgDark dark:text-bgLight hover:text-primary  w-8 h-8 rounded-l transition-all"><HiMinus/></span>
+                        <span className="flex shrink-0 items-center justify-center cursor-pointer text-primary-dark dark:text-primary-light w-8 h-8 ">{qty}</span>
+                        <span onClick={incQty} className="flex shrink-0 items-center justify-center cursor-pointer border-l  text-bgDark dark:text-bgLight hover:text-primary  w-8 h-8 rounded-r transition-all"><HiPlus/></span>
                     </span>
                 </div>
 
                 {/* buttons */}
                 <div className="w-full flex flex-wrap gap-4 justify-around  transition-all select-none" >
-                    <button type="button" onClick={() => { onAdd(product, qty) }} className="py-4 px-6 w-[220px] mt-10 text-xl cursor-pointer hover:scale-105 font-medium border bg-white border-secondary text-secondary hover:border-secondary-dark hover:text-secondary-dark hover:bg-secondary-light hover:font-semibold transition-all rounded-lg " >Add to cart</button>
+                    <button type="button" onClick={() => { onAdd(product, qty) }} className="py-4 px-6 w-[220px] mt-10 text-xl cursor-pointer hover:scale-105 font-medium border bg-white dark:bg-secondary-dark/50 border-secondary text-secondary hover:border-secondary-dark hover:text-secondary-dark hover:bg-secondary-light dark:hover:bg-secondary hover:font-semibold transition-all rounded-lg " >Add to cart</button>
                     <button type="button" onClick={handleBuyNow}  className="py-4 px-6 w-[220px] mt-10 text-xl cursor-pointer hover:scale-105 font-medium border bg-secondary-dark border-secondary-dark text-secondary-light hover:border-secondary hover:text-white hover:bg-secondary hover:font-semibold transition-all rounded-lg " >Buy now</button>
                 </div>
 
@@ -80,7 +80,7 @@ const ProductDetails = ({ product, products }) => {
         </div>
 
         <div className="mt-32  " >{/* maylike warapper */}
-            <h2 className="m-12 text-primary-dark text-2xl text-center font-semibold " >You may also like</h2>
+            <h2 className="m-12 text-primary-dark dark:text-primary-light text-2xl text-center font-semibold " >You may also like</h2>
             <div className="w-full h-[400px] relative overflow-x-hidden "> {/* maarquee text: text-[30px] text-semibold mx-16 text-secondary */}
                 <div className="flex gap-4 mt-5 justify-center w-[180%] will-change-transform absolute whitespace-nowrap animate-marquee  pause " >{/* products container */}
                     {products?.map((product) => <Product key={product._id} product={product} /> )}
